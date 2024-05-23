fb990a421fa54c87a9a2bd852fc05360255229a6
K <K>
2024-05-23 08:31:39 +0000

    ok

--- src/main.tsx
+++ src/main.tsx
@@ -6,50 +6,51 @@ import {
   RouterProvider,
 } from "react-router-dom";
 
-// import AppVite from './AppVite.tsx';
-// import App from './App.tsx';
+import AppVite from './AppVite.tsx';
+import App from './App.tsx';
 import './index.css';
 // import { account } from './libs/appwrite.ts';
 
-const router = createBrowserRouter([
+const router = createBrowserRouter(
+  [
+    {
+      path: "/",
+      element: <AppVite />,
+    },
+    {
+      path: "app",
+      element: <App />,
+      // errorElement: <ErrorPage />,
+      errorElement: <div>Error</div>,
+    },
+    // {
+    //   path: "contacts/:contactId",
+    //   element: <App />,
+    // },
+    // {
+    //   path: "protected",
+    //   element: <App />,
+    //   loader: async () => {
+    //     try {
+    //       // logged in? pass user to the route
+    //       const user = await account.get();
+    //       return { user };
+    //     }
+    //     catch {
+    //       // not logged in? redirect to login
+    //       throw redirect('/login')
+    //     }
+    //   }
+    // },
+    // {
+    //   path: "login",
+    //   element: <App />,
+    // },
+  ],
   {
-    path: "/",
-    element: <div>Test</div>,
-  },
-  // {
-  //   path: "/",
-  //   element: <AppVite />,
-  // },
-  // {
-  //   path: "app",
-  //   element: <App />,
-  //   // errorElement: <ErrorPage />,
-  //   errorElement: <div>Error</div>,
-  // },
-  // {
-  //   path: "contacts/:contactId",
-  //   element: <App />,
-  // },
-  // {
-  //   path: "protected",
-  //   element: <App />,
-  //   loader: async () => {
-  //     try {
-  //       // logged in? pass user to the route
-  //       const user = await account.get();
-  //       return { user };
-  //     }
-  //     catch {
-  //       // not logged in? redirect to login
-  //       throw redirect('/login')
-  //     }
-  //   }
-  // },
-  // {
-  //   path: "login",
-  //   element: <App />,
-  // },
-]);
+    basename: "/auth",
+  }
+);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
