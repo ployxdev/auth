c14a9ffce2b9095583247529fbdbd9b3f4884348
K <K>
2024-05-23 07:58:23 +0000

    check

--- src/main.tsx
+++ src/main.tsx
@@ -2,49 +2,49 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import {
   createBrowserRouter,
-  redirect,
+  // redirect,
   RouterProvider,
 } from "react-router-dom";
 
 import AppVite from './AppVite.tsx';
-import App from './App.tsx';
+// import App from './App.tsx';
 import './index.css';
-import { account } from './libs/appwrite.ts';
+// import { account } from './libs/appwrite.ts';
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <AppVite />,
   },
-  {
-    path: "app",
-    element: <App />,
-    // errorElement: <ErrorPage />,
-    errorElement: <div>Error</div>,
-  },
-  {
-    path: "contacts/:contactId",
-    element: <App />,
-  },
-  {
-    path: "protected",
-    element: <App />,
-    loader: async () => {
-      try {
-        // logged in? pass user to the route
-        const user = await account.get();
-        return { user };
-      }
-      catch {
-        // not logged in? redirect to login
-        throw redirect('/login')
-      }
-    }
-  },
-  {
-    path: "login",
-    element: <App />,
-  },
+  // {
+  //   path: "app",
+  //   element: <App />,
+  //   // errorElement: <ErrorPage />,
+  //   errorElement: <div>Error</div>,
+  // },
+  // {
+  //   path: "contacts/:contactId",
+  //   element: <App />,
+  // },
+  // {
+  //   path: "protected",
+  //   element: <App />,
+  //   loader: async () => {
+  //     try {
+  //       // logged in? pass user to the route
+  //       const user = await account.get();
+  //       return { user };
+  //     }
+  //     catch {
+  //       // not logged in? redirect to login
+  //       throw redirect('/login')
+  //     }
+  //   }
+  // },
+  // {
+  //   path: "login",
+  //   element: <App />,
+  // },
 ]);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
